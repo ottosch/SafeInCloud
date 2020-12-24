606fd6259e3ed2dadec8c8ade642ff4f37448082
Brennan Douglas <brennand97@gmail.com>
2020-12-23 16:41:43 -0800

    Hardcode version

--- setup.py
+++ setup.py
@@ -3,21 +3,7 @@ from setuptools import setup
 import os
 import re
 
-if os.environ.get("CI_COMMIT_TAG"):
-    version = os.environ["CI_COMMIT_TAG"]
-    if version.startswith("v"):
-        version = version[1:]
-        if not re.search(r"^\d+\.\d+\.\d+$", version):
-            raise AttributeError(
-                "given CI_COMMIT_TAG {} incorrect format. It must be vX.Y.Z or X.Y.Z format".format(
-                    os.environ["CI_COMMIT_TAG"]
-                )
-            )
-elif os.environ.get("CI_JOB_ID"):
-    version = os.environ["CI_JOB_ID"]
-else:
-    version = None
-
+version = "1.1.0"
 
 setup(
     zip_safe=True,
